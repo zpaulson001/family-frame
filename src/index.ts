331207b48ef0cecf03f6bfaace9c26ba8fa4c39f
Zac Paulson <zpaulson001@gmail.com>
2024-06-26 22:37:37 -0700

    edit hello message

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('look out world 👀');
+});
 
-export default app
+export default app;
